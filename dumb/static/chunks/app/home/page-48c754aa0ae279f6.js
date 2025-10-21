73c393f4bedaeba4cdc4d7b8e7c85861ab28d76d
jivimuz <jivirasgal@gmail.com>
2025-10-21 16:41:30 +0700

    trying update

--- dumb/static/chunks/app/home/page-48c754aa0ae279f6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[620],{752:(e,s,_)=>{Promise.resolve().then(_.bind(_,7927))}},e=>{e.O(0,[269,831,367,348,190,927,441,964,358],()=>e(e.s=752)),_N_E=e.O()}]);
